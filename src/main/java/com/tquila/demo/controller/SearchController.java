b6bc936ffd03cfca5658d94e33dddea195b313c7
Nicola Tassini <nicola@tquila.com>
2012-03-10 16:41:33 +0000

    log2

---
+++ src/main/java/com/tquila/demo/controller/SearchController.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.tquila.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tquila.demo.service.SearchService;
+
+/**
+ * @author nicolatassini
+ *
+ */
+@Controller
+@RequestMapping("/search/")
+public class SearchController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SearchController.class);
+	
+	@Autowired
+	private SearchService searchService;
+
+	@RequestMapping(value = "/indexing", method = RequestMethod.GET)
+	public String indexing(Model model) {
+		Integer indexedDocNumber = searchService.batchIndexing();
+		model.addAttribute("controllerMessage", "Indexed " + indexedDocNumber + " documents");
+		return "home";
+	}
+
+	
+}
